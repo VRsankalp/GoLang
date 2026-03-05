5be5860e956fd27a62e3ad6a1877e4293cee6916
Sankalp Srivastava <srivastavasankalp509@gmail.com>
2026-03-05 14:57:44 +0530

    variable

---
+++ variable/var.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	//in go lang if any variable is declare then it is used else delete it
+	var name string = "golang"
+	fmt.Println(name)
+
+	//implicite 
+
+	var name1 = " golags"
+	fmt.Print(name)
+	fmt.Println(name1)
+
+	//sorthand 
+	n:="golang"
+	fmt.Println(n)
+
+	// ony declare 
+	var late string	
+	late = "gigigig"
+	fmt.Println(late)
+
+}
