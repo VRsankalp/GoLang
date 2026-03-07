aaa57b30324a019f5f4ecf9084d3a2dd16b81dc6
Sankalp Srivastava <srivastavasankalp509@gmail.com>
2026-03-07 09:48:58 +0530

    range completeed

---
+++ range/range.go
@@ -0,0 +1,23 @@
+package  main
+import "fmt"
+func main(){
+// 	nums := []int{2,3,2}
+	 
+// sum:=0
+// // for idx,num:=range nums{
+// 		sum =sum+num
+	
+// 		fmt.Println(idx)
+// 	}
+// 	fmt.Println(sum)
+// }
+
+// m:=map[string]string{"hi":"ko","po":"co","ho":"bho"}
+// for k, v:=range m {
+// 	fmt.Println( k , v)
+
+//  }
+for c , v := range "golang" {
+	fmt.Println( c , v)
+}
+}
